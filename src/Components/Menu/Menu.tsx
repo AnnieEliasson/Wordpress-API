d629a7ec07824af8f5e38cb7d5b6fe046b74f227
Annie Eliasson <annie.eliasson@gmail.com>
2024-09-11 16:00:38 +0200

    fix

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { PostToWordpress } from "../../API/PostToWordpress";
 import { RetriveFromWordpress } from "../../API/RetriveFromWordpress";
 import { Article } from "../../Types/Types";
@@ -13,27 +12,32 @@ type MenuProps = {
 const Menu = ({ imageSrc, article, setArticle, setImageSrc }: MenuProps) => {
   return (
     <div className="Menu">
-      <button
-        className="menu-btn"
-        onClick={() => RetriveFromWordpress(article, setArticle, setImageSrc)}
-      >
-        Hämta
-      </button>
-      <button
-        id="draft"
-        className="menu-btn"
-        onClick={(e) => PostToWordpress(e, imageSrc, article)}
-      >
-        Spara
-      </button>
-      <button
-        id="publish"
-        className="menu-btn"
-        onClick={(e) => PostToWordpress(e, imageSrc, article)}
-      >
-        Publicera
-      </button>
-      <button className="menu-btn">Epost utskick</button>
+      <div className="button-container">
+        <button
+          className="menu-btn"
+          onClick={() => RetriveFromWordpress(article, setArticle, setImageSrc)}
+        >
+          Hämta
+        </button>
+        <button
+          id="draft"
+          className="menu-btn"
+          onClick={(e) => PostToWordpress(e, imageSrc, article)}
+        >
+          Spara
+        </button>
+        <button className="menu-btn">Välj Bild</button>
+      </div>
+      <div className="button-container">
+        <button
+          id="publish"
+          className="menu-btn"
+          onClick={(e) => PostToWordpress(e, imageSrc, article)}
+        >
+          Publicera
+        </button>
+        <button className="menu-btn">Epost utskick</button>
+      </div>
     </div>
   );
 };

40e04a956681674fce159dbcedd9e5fdceaf6247
Annie Eliasson <annie.eliasson@gmail.com>
2024-09-11 19:49:56 +0200

    context

---
+++ src/Components/Context/ArticleContextProvider.tsx
@@ -0,0 +1,44 @@
+import React, { createContext, useState, useContext } from "react";
+import { Article } from "../../Types/Types";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+type ArticleContextType = {
+  article: Article;
+  setArticle: (newArticle: Article) => void;
+};
+
+const initialArticle: Article = {
+  title: "",
+  entry: "",
+  breadth: "",
+  file: null,
+};
+
+export const ArticleContext = createContext<ArticleContextType | undefined>(
+  undefined
+);
+
+const ArticleContextProvider = ({ children }: Props) => {
+  const [article, setArticle] = useState<Article>(initialArticle);
+
+  return (
+    <ArticleContext.Provider value={{ article, setArticle }}>
+      {children}
+    </ArticleContext.Provider>
+  );
+};
+
+export const useArticleContext = () => {
+  const context = useContext(ArticleContext);
+  if (!context) {
+    throw new Error(
+      "useArticleContext måste användas inom en ArticleContextProvider"
+    );
+  }
+  return context;
+};
+
+export default ArticleContextProvider;
